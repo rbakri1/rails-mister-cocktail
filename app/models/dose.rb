f3fb18dce58016ea85307c297f0aaf01bde2ca39
Rami Bakri <rbakri1@gmail.com>
2017-05-25 16:15:03 +0100

    completed controllers

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
+  # THIS ENSURES THAT THE COCKTAIL AND INGREDIENT PAIR ARE UNIQUE
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ingredient_array = %w[lemon ice mint vodka rum gin]
+n = 0
+
+5.times do
+  Ingredient.create(
+    name: ingredient_array[n]
+    )
+  n += 1
+end
