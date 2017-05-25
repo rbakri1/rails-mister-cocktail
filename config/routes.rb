3e0fc2eef2305840c1952fe5f5f994665e6f7f1c
Rami Bakri <rbakri1@gmail.com>
2017-05-25 18:39:35 +0100

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   	end
 
   	resources :doses, only: [:destroy]
-
+  	root "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
