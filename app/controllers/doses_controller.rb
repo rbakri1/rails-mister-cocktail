8cfd4859290087d4d9f6a25de60803917f5780aa
Rami Bakri <rbakri1@gmail.com>
2017-05-26 11:13:08 +0100

    updated does routes and completed rake

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,7 +9,7 @@ class DosesController < ApplicationController
 	end
 
 	def create
-		@dose = Dose.new(dose_params)
+	    @dose = Dose.new(dose_params)
 		@dose.cocktail = @cocktail
 		if @dose.save
 		  redirect_to cocktail_path(@cocktail)
@@ -28,11 +28,11 @@ class DosesController < ApplicationController
 	private
 
 	def set_cocktail
-	  @cocktail = Cocktail.find(params[:cocktail_id])
+		@cocktail = Cocktail.find(params[:cocktail_id])
 	end
 
 	def dose_params
-	  params.require(:dose).permit(:description, :ingredient_id)
+		params.require(:dose).permit(:description, :ingredient_id)
 	end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   	end
 
   	resources :doses, only: [:destroy]
+  	# DON'T NEED DESTROY METHOD NESTED WITHIN COCKTAILS BECAUSE YOU DON'T NEED THE COCKTAIL ID IN ORDER TO DELETE THE CORRECT DOSE!
+  	# IT'S GOOD PRACTICE TO KEEP YOUR URLS AS SHORT AS POSSIBLE (I.E. NOT HAVE NEEDLESS THINGS E.G. MODELS AND IDS IN IT)
   	root "cocktails#index"
+  	# NEED TO SET THE ROOT PATH IN ORDER TO 1. UPLOAD TO HEROKU 2. HAVE A BETTER USER EXPERIENCE!
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
