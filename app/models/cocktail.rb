564f8ed741e4e813019e45d5de5017d25150fdac
Rami Bakri <rbakri1@gmail.com>
2017-05-26 14:35:11 +0100

    started styling

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,9 @@
 class Cocktail < ApplicationRecord
+	mount_uploader :photo, PhotoUploader
+
 	has_many :doses, dependent: :destroy
 	has_many :ingredients, through: :doses
 
 	validates :name, presence: true, uniqueness: true, allow_blank: false
+	validates :photo, presence: true
 end
---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+class PhotoUploader < CarrierWave::Uploader::Base
+
+  include Cloudinary::CarrierWave
+
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :standard do
+    resize_to_fit 800, 600
+  end
+
+  version :large do
+    resize_to_fit 1200, 900
+  end
+
+end
